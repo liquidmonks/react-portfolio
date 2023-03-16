c952679c82f07190422ba15e98bf06a07a02f94a
Liquid Monks <info@liquidmonks.com>
2023-03-16 18:25:19 -0500

    index.js updates the default react version 18 syntax.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css'
 
 // The ReactDOM.render method is used to render a react element into the actual DOM
 // The first argument is the component we want to render, and the second is the container element on the page
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
 
