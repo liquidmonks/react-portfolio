49e7893d580905477e9b06588874fcedae352326
Liquid Monks <info@liquidmonks.com>
2023-03-16 18:14:51 -0500

    package.js updates primary dependencies to latest build version at the time of this commit.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
 // The ReactDOM.render method is used to render a react element into the actual DOM
 // The first argument is the component we want to render, and the second is the container element on the page
 ReactDOM.render(<App />, document.getElementById('root'));
+
