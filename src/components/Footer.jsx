4fe8b92de08de07db24218d284662a5e5c33e6e9
Liquid Monks <info@liquidmonks.com>
2023-03-18 16:43:05 -0500

    src/components/Footer.jsx includes an href that redirects the user back to the home page when they click on the words Liquid Monks.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,10 +6,10 @@ export default function Footer() {
         <footer className="bg-[#454e56] p-4 lg:py-6">
             <div className="container mx-auto flex items-center justify-between flex-wrap">
                  {/* Render company name as a non-selectable text element. */}
-                <span className="select-none font-semibold text-lg lg:text-xl tracking-tight text-orange-500">Liquid Monks</span>
+                <a href="/" className="select-none font-semibold text-lg lg:text-xl tracking-tight text-orange-500">Liquid Monks</a>
                 <div className="flex space-x-3 text-lg">
                      {/* Render link to GitHub profile. */}
-                    <a href="https://github.com/" target='_blank' rel="noreferrer">
+                    <a href="https://github.com/liquidmonks?tab=repositories" target='_blank' rel="noreferrer">
                         <i className="fa-brands fa-github text-orange-500 hover:text-orange-300 duration-300"></i>
                     </a>
                      {/* Render link to LinkedIn profile. */}
